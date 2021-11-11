8362e50e834a658d3afeefbd2219bd57a771edfd
timochui <timochui@students.cs.ubc.ca>
2021-11-10 23:13:20 -0800

    added new file for insertion statements

---
+++ inserts.sql
@@ -0,0 +1,17 @@
+INSERT INTO Supplier VALUES (1234, 'Bobs food');
+INSERT INTO Supplier VALUES (1235, 'Not Bobs food');
+INSERT INTO Supplier VALUES (1236, 'Freds food');
+INSERT INTO Supplier VALUES (1237, 'We Sell Cow');
+INSERT INTO Supplier VALUES (1238, 'Vegetables Inc.');
+
+INSERT INTO Manager VALUES ('Jim', 1233, 4000);
+INSERT INTO Manager VALUES ('Jimmy', 55, 4000);
+INSERT INTO Manager VALUES ('John', 66, 4000);
+INSERT INTO Manager VALUES ('Johnny', 77, 4000);
+INSERT INTO Manager VALUES ('Jason', 88, 4000);
+
+INSERT INTO preparesOrder VALUES (112, 1245);
+INSERT INTO preparesOrder VALUES (113, 1245);
+INSERT INTO preparesOrder VALUES (114, 1245);
+INSERT INTO preparesOrder VALUES (115, 1245);
+INSERT INTO preparesOrder VALUES (116, 1245);
