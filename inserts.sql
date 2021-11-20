12b2581a6be0ed4a2cecf7bd931a1e5db5bf26ab
timochui <timochui@students.cs.ubc.ca>
2021-11-19 16:30:34 -0800

    fixed orders of insertions

--- inserts.sql
+++ inserts.sql
@@ -52,72 +52,70 @@ INSERT INTO Collects VALUES (1233, 3);
 INSERT INTO Collects VALUES (1233, 4);
 INSERT INTO Collects VALUES (1233, 5);
 
--- INSERT INTO preparesOrder VALUES (112, 1333);
--- INSERT INTO preparesOrder VALUES (113, 1344);
--- INSERT INTO preparesOrder VALUES (114, 1355);
--- INSERT INTO preparesOrder VALUES (115, 1366);
--- INSERT INTO preparesOrder VALUES (116, 1377);
+INSERT INTO CustomerR1 VALUES ('Bob', 2, '1234567890');
+INSERT INTO CustomerR1 VALUES ('Bobby', 2, '2345678901');
+INSERT INTO CustomerR1 VALUES ('Bobbette', 3, '3456789012');
+INSERT INTO CustomerR1 VALUES ('Bobnathon', 4, '4567890123');
+INSERT INTO CustomerR1 VALUES ('Bobu', 5, '5678901234');
+
+INSERT INTO CustomerR2 VALUES (100, 'Bob', '1234567890');
+INSERT INTO CustomerR2 VALUES (200, 'Bobby', '2345678901');
+INSERT INTO CustomerR2 VALUES (300, 'Bobbette', '3456789012');
+INSERT INTO CustomerR2 VALUES (400, 'Bobnathon', '4567890123');
+INSERT INTO CustomerR2 VALUES (500, 'Bobu', '5678901234');
+
+INSERT INTO CustomerR3 VALUES ('Bob', '9:00', '1234567890');
+INSERT INTO CustomerR3 VALUES ('Bobby', '9:00', '2345678901');
+INSERT INTO CustomerR3 VALUES ('Bobbette', '9:00', '3456789012');
+INSERT INTO CustomerR3 VALUES ('Bobnathon', '9:00', '4567890123');
+INSERT INTO CustomerR3 VALUES ('Bobu', '10:00', '5678901234');
+
+INSERT INTO Drink VALUES ('lady mary', 8);
+INSERT INTO Drink VALUES ('gin gin', 20);
+INSERT INTO Drink VALUES ('apple juice', 5);
+INSERT INTO Drink VALUES ('coke', 0);
+INSERT INTO Drink VALUES ('tiger bomb', 12);
+
+INSERT INTO Pays VALUES (1, 100);
+INSERT INTO Pays VALUES (2, 200);
+INSERT INTO Pays VALUES (3, 300);
+INSERT INTO Pays VALUES (4, 400);
+INSERT INTO Pays VALUES (5, 500);
+
+INSERT INTO hasRestaurant VALUES (11, 'happy rd', 1233);
+INSERT INTO hasRestaurant VALUES (22, 'unhappy rd', 1233);
+INSERT INTO hasRestaurant VALUES (33, 'sad rd', 1233);
+INSERT INTO hasRestaurant VALUES (44, 'excited ave', 1233);
+INSERT INTO hasRestaurant VALUES (55, 'angry rd', 1233);
 
 
--- INSERT INTO givesOrderOrder VALUES (112, 1433, 100);
--- INSERT INTO givesOrderOrder VALUES (112, 1433, 200);
--- INSERT INTO givesOrderOrder VALUES (112, 1433, 300);
--- INSERT INTO givesOrderOrder VALUES (112, 1466, 400);
--- INSERT INTO givesOrderOrder VALUES (112, 1477, 500);
+INSERT INTO containsFoodItemR1 VALUES ('bob wings', 'it has chicken');
+INSERT INTO containsFoodItemR1 VALUES ('Steak', 'From a pig');
+INSERT INTO containsFoodItemR1 VALUES ('baked salmon', 'fresh salmon');
+INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 'it''s a cow');
+INSERT INTO containsFoodItemR1 VALUES ('Jim''s surprise', 'salad');
 
--- INSERT INTO Drink VALUES ('lady mary', 8);
--- INSERT INTO Drink VALUES ('gin gin', 20);
--- INSERT INTO Drink VALUES ('apple juice', 5);
--- INSERT INTO Drink VALUES ('coke', 0);
--- INSERT INTO Drink VALUES ('tiger bomb', 12);
+INSERT INTO containsFoodItemR3 VALUES ('bob wings', 'Chicken Wings');
+INSERT INTO containsFoodItemR3 VALUES ('Steak', 'Pork Chops');
+INSERT INTO containsFoodItemR3 VALUES ('baked salmon', 'Salmon');
+INSERT INTO containsFoodItemR3 VALUES ('ribeye steak', 'Ribeye');
+INSERT INTO containsFoodItemR3 VALUES ('Jim''s surprise', 'Broccoli');
 
--- INSERT INTO Pays VALUES (1, 100);
--- INSERT INTO Pays VALUES (2, 200);
--- INSERT INTO Pays VALUES (3, 300);
--- INSERT INTO Pays VALUES (4, 400);
--- INSERT INTO Pays VALUES (5, 500);
+INSERT INTO givesOrderOrder VALUES (112, 1433, 100);
+INSERT INTO givesOrderOrder VALUES (113, 1433, 200);
+INSERT INTO givesOrderOrder VALUES (114, 1433, 300);
+INSERT INTO givesOrderOrder VALUES (115, 1466, 400);
+INSERT INTO givesOrderOrder VALUES (116, 1477, 500);
 
--- INSERT INTO hasRestaurant VALUES (11, 'happy rd', 1233);
--- INSERT INTO hasRestaurant VALUES (22, 'unhappy rd', 1233);
--- INSERT INTO hasRestaurant VALUES (33, 'sad rd', 1233);
--- INSERT INTO hasRestaurant VALUES (44, 'excited ave', 1233);
--- INSERT INTO hasRestaurant VALUES (55, 'angry rd', 1233);
+INSERT INTO containsFoodItemR2 VALUES ('bob wings', 10, 112);
+INSERT INTO containsFoodItemR2 VALUES ('Steak', 20, 113);
+INSERT INTO containsFoodItemR2 VALUES ('baked salmon', 30, 114);
+INSERT INTO containsFoodItemR2 VALUES ('ribeye steak', 20, 115);
+INSERT INTO containsFoodItemR2 VALUES ('Jim''s surprise', 10, 116);
+INSERT INTO containsFoodItemR2 VALUES ('ribeye steak', 20, 116);
 
--- INSERT INTO CustomerR1 VALUES ('Bob', 2, '1234567890');
--- INSERT INTO CustomerR1 VALUES ('Bobby', 2, '2345678901');
--- INSERT INTO CustomerR1 VALUES ('Bobbette', 3, '3456789012');
--- INSERT INTO CustomerR1 VALUES ('Bobnathon', 4, '4567890123');
--- INSERT INTO CustomerR1 VALUES ('Bobu', 5, '5678901234');
-
--- INSERT INTO CustomerR2 VALUES (100, 'Bob', '1234567890');
--- INSERT INTO CustomerR2 VALUES (200, 'Bobby', '2345678901');
--- INSERT INTO CustomerR2 VALUES (300, 'Bobbette', '3456789012');
--- INSERT INTO CustomerR2 VALUES (400, 'Bobnathon', '4567890123');
--- INSERT INTO CustomerR2 VALUES (500, 'Bobu', '5678901234');
-
--- INSERT INTO CustomerR3 VALUES ('Bob', '9:00', '1234567890');
--- INSERT INTO CustomerR3 VALUES ('Bobby', '9:00', '2345678901');
--- INSERT INTO CustomerR3 VALUES ('Bobbette', '9:00', '3456789012');
--- INSERT INTO CustomerR3 VALUES ('Bobnathon', '9:00', '4567890123');
--- INSERT INTO CustomerR3 VALUES ('Bobu', '10:00', '5678901234');
-
--- INSERT INTO containsFoodItemR1 VALUES ('bob wings', 'it has chicken');
--- INSERT INTO containsFoodItemR1 VALUES ('Steak', 'From a pig');
--- INSERT INTO containsFoodItemR1 VALUES ('baked salmon', 'fresh salmon');
--- INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 'it''s a cow');
--- INSERT INTO containsFoodItemR1 VALUES ('Jim''s surprise', 'salad');
--- INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 'it''s a cow');
-
--- INSERT INTO containsFoodItemR1 VALUES ('bob wings', 10, 112);
--- INSERT INTO containsFoodItemR1 VALUES ('Steak', 20, 113);
--- INSERT INTO containsFoodItemR1 VALUES ('baked salmon', 30, 114);
--- INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 20, 115);
--- INSERT INTO containsFoodItemR1 VALUES ('Jim''s surprise', 10, 116);
--- INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 20, 116);
-
--- INSERT INTO containsFoodItemR1 VALUES ('bob wings', 'Chicken Wings');
--- INSERT INTO containsFoodItemR1 VALUES ('Steak', 'Pork Chops');
--- INSERT INTO containsFoodItemR1 VALUES ('baked salmon', 'Salmon');
--- INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 'Ribeye');
--- INSERT INTO containsFoodItemR1 VALUES ('Jim''s surprise', 'Broccoli');
--- INSERT INTO containsFoodItemR1 VALUES ('ribeye steak', 'Ribeye');
+INSERT INTO preparesOrder VALUES (112, 1333);
+INSERT INTO preparesOrder VALUES (113, 1344);
+INSERT INTO preparesOrder VALUES (114, 1355);
+INSERT INTO preparesOrder VALUES (115, 1366);
+INSERT INTO preparesOrder VALUES (116, 1377);
